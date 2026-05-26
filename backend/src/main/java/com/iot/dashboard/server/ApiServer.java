c2df120cce5a307ce91d333868cc7f2009579ba0
Anastasia Shkundich <shkundich30@gmail.com>
2026-05-26 19:26:09 +0300

    ApiServer created

---
+++ backend/src/main/java/com/iot/dashboard/server/ApiServer.java
@@ -0,0 +1,54 @@
+package com.iot.dashboard.server;
+
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import com.iot.dashboard.api.NodeController;
+import com.iot.dashboard.api.TelemetryController;
+import com.iot.dashboard.db.TelemetryRepository;
+import com.iot.dashboard.simulation.TelemetrySimulator;
+import io.javalin.Javalin;
+import io.javalin.json.JavalinJackson;
+
+public class ApiServer {
+    private final TelemetryRepository repository;
+    private final TelemetrySimulator simulator;
+    private Javalin app;
+
+    public ApiServer(TelemetryRepository repository, TelemetrySimulator simulator) {
+        this.repository = repository;
+        this.simulator = simulator;
+    }
+
+    public void start(int port) {
+        app = Javalin.create(config -> {
+            // CORS
+            config.bundledPlugins.enableCors(cors -> {
+                cors.addRule(rule -> {
+                    rule.anyHost(); // for development only
+                });
+            });
+
+            // handling Java 8 date/time types
+            config.jsonMapper(new JavalinJackson().updateMapper(mapper -> {
+                mapper.registerModule(new JavaTimeModule());
+            }));
+
+            config.requestLogger.http((ctx, ms) ->
+                    System.out.println(ctx.method() + " " + ctx.path() + " → " + ctx.status() + " (" + ms + "ms)")
+            );
+        });
+
+        registerRoutes();;
+        app.start(port);
+    }
+
+    public void stop() {
+        if (app != null) {
+            app.stop();
+        }
+    }
+
+    private void registerRoutes() {
+        new NodeController(repository, simulator).registerRoutes(app);
+        new TelemetryController(repository).registerRoutes(app);
+    }
+}
